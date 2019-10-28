77c52e6c21fde8f19d37afa8a32ff410a19f5851
Artem <Osipchik@tut.by>
2019-10-28 23:05:20 +0300

    Delete UserService.java

--- src/by/epam/accounting_finance/accounting_finance/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package by.epam.accounting_finance.service;
-
-
-import by.epam.accounting_finance.beans.User;
-
-public interface UserService {
-    User authorization (String login, String password) throws ServiceException;
-}
