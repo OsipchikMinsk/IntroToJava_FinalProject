e37493d69dce3c658d0a9392dc22463fb40587e6
Artem <Osipchik@tut.by>
2019-10-28 23:12:25 +0300

    Delete SerializationService.java

--- src/by/epam/accounting_finance/service/SerializationService.java
+++
@@ -1,9 +0,0 @@
-package by.epam.accounting_finance.service;
-
-import by.epam.accounting_finance.beans.User;
-
-import java.util.List;
-
-public interface SerializationService {
-    User getUserFromFile(String name);
-}
